991c4bb6250d1bd9290d42a2014f7efc5fece95f
niharikachawla24 <nchawla_be24@thapar.edu>
2025-09-03 13:12:15 +0530

    Create Additional Question 4.cpp

---
+++ Additional Question 4.cpp
@@ -0,0 +1,43 @@
+//Additional Question 4
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+    int arr[n];
+    
+    cout << "Enter " << n << " elements (only 0, 1, 2):\n";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int low = 0;
+    int mid = 0;
+    int high = n - 1;
+
+    while (mid <= high) {
+        if (arr[mid] == 0) {
+            int temp = arr[low];
+            arr[low] = arr[mid];
+            arr[mid] = temp;
+            low++;
+            mid++;
+        } 
+        else if (arr[mid] == 1) {
+            mid++;
+        } 
+        else {
+            int temp = arr[mid];
+            arr[mid] = arr[high];
+            arr[high] = temp;
+            high--;
+        }
+    }
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+}
